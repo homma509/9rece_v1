a9c9b082afea9864cf36513e7824778a6fabcb96
homma509 <kouki.honma@gmail.com>
2020-11-14 13:50:01 +0900

    rename directory name api to cmd

--- server/scripts/build.sh
+++ server/scripts/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd api
+cd cmd
 find . -name main.go -type f \
  | xargs -n 1 dirname \
  | xargs -n 1 -I@ bash -c "CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -v -o ../build/@/main ./@"
